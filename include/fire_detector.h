7721b1aa71e610783f5dafa2c21814a45a394093
Jacob Nash <jacob.matthew.nash@gmail.com>
2023-06-01 00:38:33 +0100

    Updated to testing yolov7

--- include/fire_detector.h
+++ include/fire_detector.h
@@ -3,6 +3,8 @@
 
 #include <ros/ros.h>
 #include <sensor_msgs/Image.h>
+#include <ctime>
+#include <chrono>
 
 class fire_detector{
     public:
@@ -11,12 +13,15 @@ class fire_detector{
         ros::NodeHandle nh_;
         ros::Subscriber cam_sub_;
         ros::Subscriber detect_sub_;
-
+        std::chrono::steady_clock::time_point current_time_;
+        std::chrono::steady_clock::time_point last_time_;
         ros::Publisher img_pub_;
+
 
         void cameraCallback(const sensor_msgs::Image::ConstPtr &msg);
 
         void detectionCallback(const sensor_msgs::Image::ConstPtr &msg);
+        
 };
 
 #endif
--- src/fire_detector.cpp
+++ src/fire_detector.cpp
@@ -4,10 +4,12 @@ fire_detector::fire_detector(ros::NodeHandle nh){
     nh_ = nh;
     // Initialise ROS Subscribers
     cam_sub_ = nh_.subscribe("/usb_cam/image_raw",1,&fire_detector::cameraCallback, this);
-    detect_sub_ = nh_.subscribe("darknet_ros/detection_image",1,&fire_detector::detectionCallback,this);
+    detect_sub_ = nh_.subscribe("/yolov7/yolov7/visualization",1,&fire_detector::detectionCallback,this);
 
     // Define the publishers
     img_pub_ = nh_.advertise<sensor_msgs::Image>("/camera/rgb/image_raw", 1, true);
+    last_time_ = std::chrono::steady_clock::now();
+    current_time_ = std::chrono::steady_clock::now();
 }
 
 void fire_detector::cameraCallback(const sensor_msgs::Image::ConstPtr& msg)
@@ -20,6 +22,14 @@ void fire_detector::cameraCallback(const sensor_msgs::Image::ConstPtr& msg)
 
 void fire_detector::detectionCallback(const sensor_msgs::Image::ConstPtr &msg){
     ROS_INFO("Object Found");
+    current_time_ = std::chrono::steady_clock::now();
+    std::chrono::steady_clock::duration time_span = current_time_ - last_time_;
+    double nseconds = double(time_span.count()) * std::chrono::steady_clock::steady_clock::period::num / std::chrono::steady_clock::steady_clock::period::den;
+    float htz = 1/nseconds;
+    ROS_INFO("htz %f", htz);
+    last_time_ = std::chrono::steady_clock::now();
     return;
 
 }
+
+
